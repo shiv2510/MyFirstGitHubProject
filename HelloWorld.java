50236e2cbd6e97676d308d49a9a721067732ef86
shiv2510 <shivec0311@gmail.com>
2019-05-08 11:40:18 -0400

    compiled the java progran using Jenkins and git

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args){
-	System.out.println("Hello Worlld");
-}
+
+		for(i=1;i<=10;i++){	
+			System.out.println("Hello World "+i);
+		}
+	}
 }
